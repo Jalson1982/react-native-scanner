2c8a48f029b70e4036956ea42099dc93980bd5d9
Jalson1982 <jasmin.fajkic@gmail.com>
2025-01-19 15:27:31 +0100

    Enable on IOS

--- ios/ReactNativeScannerView.h
+++ ios/ReactNativeScannerView.h
@@ -1,5 +1,4 @@
-// This guard prevent this file to be compiled in the old architecture.
-#ifdef RCT_NEW_ARCH_ENABLED
+
 #import <React/RCTViewComponentView.h>
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
